358a0885d4933beb75816c56da691cc3bc49c306
StanislavMikheyev <stanislav.mikheyev@gmail.com>
2017-01-06 21:58:43 +0100

    Add .gitignore and minor refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,22 @@
 public class Main
 {
+
+    enum GESTURE {
+        ROCK, PAPER, SCISSORS
+    };
+
     public static void main(String[] args)
     {
-        int me = 0;
-        int op = 1;
-        int len = 5;
+        int me = GESTURE.ROCK.ordinal();
+        int op = GESTURE.SCISSORS.ordinal();
+        int len = 3;
 
         int opposit = (me + len / 2) % len;
-
-        boolean answer = false;
 
         if (me == op) {
             System.out.println("Draw");
             return;
         }
-
-
-
-    /*if (me < opposit) {
-      answer = (me < op) && (op <= opposit);
-    } else {
-      answer = (me < op) || (op <= opposit);
-    }
-
-    System.out.println(!answer);*/
 
         boolean B = me >= op;
         boolean C = op > opposit;
